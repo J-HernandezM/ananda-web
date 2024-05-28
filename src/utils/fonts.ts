7abd44b1eb8021854ea12d703850bfe40437df4c
Juan <juansw03@gmail.com>
2024-05-28 17:04:21 -0500

    feat: brand fonts configuration

---
+++ src/utils/fonts.ts
@@ -0,0 +1,16 @@
+import localFont from 'next/font/local';
+import { Lato } from 'next/font/google';
+
+export const lato = Lato({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  style: ['normal', 'italic'],
+  variable: '--font-lato',
+});
+
+export const absolutely_vintage = localFont({
+  src: '../../public/fonts/Absolutely-Vintage.otf',
+  style: 'normal',
+  weight: '400',
+  variable: '--font-absolutely',
+});
