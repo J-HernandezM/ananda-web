37f3a77351aadf07daf260efeacee66ee42aa2a1
Juan <juansw03@gmail.com>
2024-06-12 01:33:26 -0500

    feat: new featured section with its structure, styles and functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
+import { redirect } from 'next/navigation';
 import bgPhoto from '@/assets/images/home/header-home.webp';
+import arrowIcon from '@/assets/svg/icons-right-arrow.svg';
 import BackgroundTopPhoto from '@/components/BackgroundTopPhoto';
+import FeaturedButton from '@/components/FeaturedButton';
+import './home.scss';
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
-      <BackgroundTopPhoto src={bgPhoto} />
+      <section className="featured">
+        <BackgroundTopPhoto src={bgPhoto} />
+        <div className="featured--content">
+          <h2 className="featured--title">Cuidamos de ti</h2>
+          <p className="featured--subtitle">
+            con las mejores formulas y los mejores ingredientes
+          </p>
+          <FeaturedButton
+            onclick={navigateToProducts}
+            text="Explorar productos"
+            image={arrowIcon}
+          />
+        </div>
+      </section>
     </main>
   );
 }
+
+const navigateToProducts = async () => {
+  'use server';
+  redirect('/tienda');
+};
