c6432ce5b38b78ba3b925629e1a8700e691a0650
Juan <juansw03@gmail.com>
2024-06-18 16:01:48 -0500

    fix: some routes had its featured images incorrectly positioned on some viewports
    /productos and /preguntas-frecuentes

--- src/app/preguntas-frecuentes/page.tsx
+++ src/app/preguntas-frecuentes/page.tsx
@@ -1,5 +1,6 @@
 import bgPhoto from '@/assets/images/preguntas-frecuentes/header-faq.webp';
 import BackgroundTopPhoto from '@/components/BackgroundTopPhoto';
+import './faqPage.scss';
 
 export default function FaqPage() {
   return (
