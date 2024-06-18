50bdcccf7dcd086b784b0bd1004302bd81485779
Juan <juansw03@gmail.com>
2024-06-18 18:37:45 -0500

    test(header): correcting routing test at header to avoid false positives

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -41,20 +41,27 @@ describe('<Header />', () => {
     });
   });
 
-  // TODO: confirm that this is a correct approach
-  test('Click at a Link should lead to a different route', async () => {
+  // TODO: consult if this approach is correct
+  // Explanation: without the change in href we get console.error wich is a test-only warning from a non implemented yet feature with JSDOM (navigation)
+  test('Click on a Link should produce a re render (in to a different route)', async () => {
     // Arrange
     const link = screen.getAllByRole('link', { name: 'Nosotros' })[0];
-    (usePathname as jest.Mock).mockReturnValue('/nosotros');
+    // Ensure link is an Anchor element
+    if (!(link instanceof HTMLAnchorElement)) { return; }
+
+    // To avoid console.error about jsdom not having navigation yet
+    link.href = `#${link.href.split('/').at(-1)}`;
 
     // Act
     await act(() => {
+      // Make the navigation (should produce a re-render)
       userEvent.click(link);
     });
 
     // Assert
     await waitFor(() => {
-      expect(usePathname()).toContain('/nosotros');
+      // If re-render happens to occur, then usePathname should be called
+      expect(usePathname).toHaveBeenCalled();
     });
   });
 });
