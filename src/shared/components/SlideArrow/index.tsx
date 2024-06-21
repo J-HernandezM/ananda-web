65ce6f962a219a9096d9703dc5111373f100cc40
Juan <juansw03@gmail.com>
2024-06-21 02:57:16 -0500

    feat: slider functionalities implemented
    made by using react-slick librarie and adjusting styles to the librarie requirements

---
+++ src/shared/components/SlideArrow/index.tsx
@@ -0,0 +1,23 @@
+import arrowIcon from '@/assets/svg/icons-arrow.svg';
+import Image from 'next/image';
+import { CustomArrowProps } from 'react-slick';
+import './slideArrow.scss';
+
+interface ArrowProps extends CustomArrowProps {
+  direction: 'next' | 'prev';
+}
+
+export default function SlideArrow({ direction, onClick }: ArrowProps) {
+  return (
+    <Image
+      src={arrowIcon}
+      alt={
+        direction === 'next'
+          ? 'flecha siguiente producto'
+          : 'flecha anterior producto'
+      }
+      className={`${direction == 'next' ? 'carousel--arrow-right' : 'carousel--arrow-left'} carousel--arrow`}
+      onClick={onClick}
+    />
+  );
+}
