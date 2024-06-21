3e2d27513ae18fe0eec8c18724d8d0880dc25862
Juan <juansw03@gmail.com>
2024-06-21 16:28:48 -0500

    feat: small changes about icons and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,23 @@ export const metadata: Metadata = {
   title: 'Ananda Jaboneria Artesanal',
   description:
     'Somos un taller de cosmética artesanal realizada con ingredientes naturales, de origen vegetal, sin sustancias químicas nocivas para la piel y el medio ambiente. Nuestro objetivo es crear un producto hecho en casa, hecho a mano, hecho con amor, con ingredientes naturales altamente hidratantes, humectantes, nutritivos, reparadores y terapeuticos que sean amigables con el medio ambiente. Utilizamos aceites vegetales prensados en frio, extractos de plantas, macerados, fragancias de aceites esenciales y aditivos como cafe, cacao, avena, pulpas de fruta, semillas secas y leches vegetales de procedencia organica, entre otros. Estamos comprometidos con el bienestar y la salud de la piel, entregando productos naturales de alta calidad y que brindan salud y belleza',
+  keywords: [
+    'Jabon de glicerina',
+    'Jabones de glicerina',
+    'Cosmetica natural',
+    'cuidado de la piel',
+  ],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html
-      className={`${fonts.lato.variable} ${fonts.absolutely_vintage.variable}`}
-      lang="en"
-    >
+    <html className={`${fonts.lato.variable} ${fonts.absolutely_vintage.variable}`} lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="./icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="./icons/site.webmanifest" />
         <link
           rel="stylesheet"
           type="text/css"
