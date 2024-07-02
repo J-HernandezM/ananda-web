64a9f1f2dc14b4c20e82c48693bf0e45ebd2f279
Juan <juansw03@gmail.com>
2024-07-02 12:45:50 -0500

    chore: icons and webmanifest moved to public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html className={`${fonts.lato.variable} ${fonts.absolutely_vintage.variable}`} lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="./icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="./icons/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="stylesheet"
           type="text/css"
