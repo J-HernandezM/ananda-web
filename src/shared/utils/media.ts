848f8fa1536e38b3c88d84354002da973467b962
Juan <juansw03@gmail.com>
2024-07-18 15:30:12 -0500

    feat: new component created to list the different social media icons and links

---
+++ src/shared/utils/media.ts
@@ -0,0 +1,17 @@
+import mediaFace from '@/assets/svg/media-facebook.svg';
+import mediaInsta from '@/assets/svg/media-instagram.svg';
+
+const media = [
+  {
+    title: 'facebook',
+    img: mediaFace.src,
+    url: 'https://www.facebook.com/p/Ananda-jaboneria-artesanal-100063548289764/',
+  },
+  {
+    title: 'instagram',
+    img: mediaInsta.src,
+    url: 'https://www.instagram.com/anandajaboneriartesanal/',
+  },
+];
+
+export default media;
