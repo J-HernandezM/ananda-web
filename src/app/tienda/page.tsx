fa9bf0a0e9ca6c81f6066ced095e849b4c2d13bc
Juan <juansw03@gmail.com>
2024-08-07 22:55:20 -0500

    feat: store page structure and first approach in styling

--- src/app/tienda/page.tsx
+++ src/app/tienda/page.tsx
@@ -1,11 +1,31 @@
 import bgPhoto from '@/assets/images/tienda/header-tienda.webp';
 import BackgroundTopPhoto from '@/components/BackgroundTopPhoto';
 import './storePage.scss';
+import MobileCategoriesFilter from '@/components/CategoriesFilter/MobileCategoriesFilter';
+import CategoriesFilter from '@/components/CategoriesFilter';
 
 export default function StorePage() {
   return (
     <main>
       <BackgroundTopPhoto src={bgPhoto} />
+      <section className="store">
+        <aside className="store--aside">
+          <MobileCategoriesFilter></MobileCategoriesFilter>
+          <CategoriesFilter></CategoriesFilter>
+        </aside>
+        <article className="store--products">
+          <div className="store--order">
+            <p>Ordenar por:</p>
+            <select name="order" id="store--order-select">
+              <option value="selled">Más vendido</option>
+              <option value="recent">Más reciente</option>
+              <option value="price-higher">Precio más alto</option>
+              <option value="price-lower">Precio más bajo</option>
+            </select>
+          </div>
+          <div className="store--grid"></div>
+        </article>
+      </section>
     </main>
   );
 }
