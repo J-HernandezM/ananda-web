31db8e289fed3b508f88177435dd69b5027564eb
Juan <juansw03@gmail.com>
2024-08-16 22:22:21 -0500

    feat: added the correct properties and methods to the cart store

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,26 +1,57 @@
+import { ProductTemporalInterface } from '@/shared/utils/mockedProducts';
 import { create } from 'zustand';
 
+export type Order = {
+  id: string;
+  product: ProductTemporalInterface;
+  promo: Promo;
+  quantity: number;
+};
+
+export type Promo = 1 | 3 | 12;
+
 export type CartState = {
-  count: number;
+  total: number;
+  orders: Order[];
 };
 
 export type CartActions = {
-  decrementCount: () => void;
-  incrementCount: () => void;
+  addToCart: (order: Order) => void;
+  removeFromCart: (id: string) => void;
+  updateQuantity: (id: string, quantity: number) => void;
 };
 
 export type CartStore = CartState & CartActions;
 
 export const useCartStore = create<CartStore>(set => ({
-  count: new Date().getFullYear(),
-  incrementCount: () => {
+  total: 0,
+  orders: [],
+  addToCart: newOrder => {
     set(state => {
-      return { count: state.count + 1 };
+      const orders = [...state.orders, newOrder];
+      const total = getTotal(orders);
+
+      return { orders, total };
     });
   },
-  decrementCount: () => {
+  removeFromCart: id => {
     set(state => {
-      return { count: state.count - 1 };
+      const orders = state.orders.filter(order => order.id === id);
+      const total = getTotal(orders);
+
+      return { orders, total };
+    });
+  },
+  updateQuantity: (id, quantity) => {
+    set(state => {
+      const orders = state.orders.map(order => (order.id === id ? { ...order, quantity } : order));
+      const total = getTotal(orders);
+
+      return { orders, total };
     });
   },
 }));
+
+function getTotal(orders: Order[]): number {
+  return orders.reduce((acc, curr) => acc + curr.product.price * curr.quantity, 0);
+}
