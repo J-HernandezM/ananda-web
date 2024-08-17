e911cd4a13f6f3323aeaa3fe779e34171da454b6
Juan <juansw03@gmail.com>
2024-08-16 20:50:00 -0500

    feat: added the mock strapi response, with a sanitizer function and its typing

---
+++ src/shared/utils/tests/sanitizeApiResponse.test.js
@@ -0,0 +1,43 @@
+import { sanitizeApiResponse } from '../sanitizeApiResponse';
+import mockStrapiResponse from '../mockedStrapiResponse.json';
+
+describe('sanitizeApiResponse', () => {
+  test('Should map correctly', () => {
+    const imageMatcher = expect.objectContaining({
+      url: expect.any(String),
+      width: expect.any(Number),
+      height: expect.any(Number),
+      alternativeText: expect.any(String),
+    });
+
+    const expected = expect.objectContaining({
+      title: expect.any(String),
+      description: expect.any(String),
+      id: expect.any(Number),
+      images: expect.arrayContaining([imageMatcher]),
+      featuredImage: imageMatcher,
+    });
+
+    const result = sanitizeApiResponse(mockStrapiResponse);
+
+    expect(result).toEqual(expect.arrayContaining([expected]));
+  });
+
+  it('Should be empty if API response is invalidad', () => {
+    const invalidData = { data: undefined };
+
+    const expected = undefined;
+    const result = sanitizeApiResponse(invalidData);
+
+    expect(expected).toBe(result);
+  });
+
+  it('Should be empty if API response is not an array', () => {
+    const invalidData = { data: 123123 };
+
+    const expected = undefined;
+    const result = sanitizeApiResponse(invalidData);
+
+    expect(expected).toBe(result);
+  });
+});
